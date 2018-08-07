733ec4d8c790a99f2e06f55a9afe78ec7430c7fd
Rendy Ichtiar <rendyichtiar25@gmail.com>
2018-08-07 11:27:16 +0700

    new

---
+++ resources/views/page_navigation.blade.php
@@ -0,0 +1,35 @@
+<div class="sidebar" data-color="purple" data-background-color="white" data-image="./public/assets/img/sidebar-1.jpg">
+      <div class="logo">
+        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+          AKB PROJECT V1
+        </a>
+      </div>
+      <div class="sidebar-wrapper">
+        <ul class="nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="{{ url('/') }}">
+              <i class="material-icons">dashboard</i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="material-icons">receipt</i>
+              <p>Audit</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="material-icons">table_chart</i>
+              <p>Barang</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="material-icons">add_shopping_cart</i>
+              <p>Kasir</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
